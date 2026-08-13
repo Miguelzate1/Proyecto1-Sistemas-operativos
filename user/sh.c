e42dc64aaeb11c6ae0228320dfa7b338c1a37433
Miguelzate1 <alzatem114@gmail.com>
2026-08-13 00:16:50 -0500

    feat(exec): implementada ejecucion simple de comandos con exec

--- user/sh.c
+++ user/sh.c
@@ -1,6 +1,7 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+int strlen(const char*);
 int getcmd(char *buf, int nbuf) {
   fprintf(2, "$ ");
   memset(buf, 0, nbuf);
@@ -14,6 +15,7 @@ int main(void) {
   static char buf[100];
 
 
+
   while(getcmd(buf, sizeof(buf)) >= 0) {
     int pid =  fork ();
 
@@ -21,14 +23,23 @@ int main(void) {
       fprintf(2, "sh: error en fork\n");
       continue;
     }
+    
+    if (pid ==0){
+      if(buf [strlen(buf)-1]== '\n')
+      buf [strlen(buf)-1]=0;
 
-    if (pid == 0)
-{
-  fprintf(2,"Proceso hijo creado con exito\n");
-  exit(0);
-}
+      char *argv[2];
+      argv[0]=buf;
+      argv[1]=0;
+      
+      exec(argv[0],argv);
 
-wait(0);
+      fprintf(2,"sh: exec %s fallo\n",buf);
+      exit(1);
+
+    }
+    wait(0);
   }
+
   exit(0);
 }
