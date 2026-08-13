4c4693411b188124b569ff8de4768665eee1f7d9
Miguelzate1 <alzatem114@gmail.com>
2026-08-12 20:36:08 -0500

    feat(exec): agregada estructura inicial de fork y wait

--- user/sh.c
+++ user/sh.c
@@ -13,9 +13,22 @@ int getcmd(char *buf, int nbuf) {
 int main(void) {
   static char buf[100];
 
+
   while(getcmd(buf, sizeof(buf)) >= 0) {
-    fprintf(2, "Comando recibido: %s", buf);
+    int pid =  fork ();
+
+    if(pid < 0){
+      fprintf(2, "sh: error en fork\n");
+      continue;
+    }
+
+    if (pid == 0)
+{
+  fprintf(2,"Proceso hijo creado con exito\n");
+  exit(0);
+}
+
+wait(0);
   }
-  
   exit(0);
 }
